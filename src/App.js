a53bfe1ddea9fb7dd3a0d7e5f6af7e4081b4c131
Prince Sharma <prince12845sharma@gmail.com>
2024-02-21 21:33:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Work from "./components/Work";
 import Timeline from "./components/Timeline";
 import Services from "./components/Services";
+import LoginPage from "./components/LoginPage";
 // import Testimonails from "./components/Testimonails";
 import Contact from "./components/Contact";
 import Footer  from "./components/Footer.jsx";
@@ -17,6 +18,7 @@ function App() {
   console.log(menuOpen);
   return (
 <>
+    <LoginPage />
     <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     <Home />
